283b81c0151fae628947858aeb11dc8257ab6ba3
Timothy Pakkianathan <tim@pakkianathan.com>
2013-11-29 23:05:03 +1100

    Initial commit.
    Added the Project "Mini Browser" - Completed
    Added the Project "Pet Sounds" - WIP

---
+++ MiniBrowser/MiniBrowser/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MiniBrowser.Resources;
+
+namespace MiniBrowser
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
