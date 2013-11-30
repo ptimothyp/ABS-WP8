3f674f3a614fcc20800b5d2eca536c072c772a1f
Timothy Pakkianathan <tim@pakkianathan.com>
2013-11-30 14:54:38 +1100

    Getting in the demo projects StarSizing - Demo of how to use * sizing in Grid layout Alignment and Margins - Demos the use of margins in Grid layout.

---
+++ AlignmentAndMargins/AlignmentAndMargins/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AlignmentAndMargins.Resources;
+
+namespace AlignmentAndMargins
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
